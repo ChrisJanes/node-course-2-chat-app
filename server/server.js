718082eee820f045416af045e038f6a70a959ae6
Chris Janes <gingoreilly@gmail.com>
2017-10-16 20:00:22 +0100

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('new user connected');
 
-  socket.emit('newMessage', {
-    from: 'chris',
-    text: 'This is my text',
-    createdAt: 123
-  });
-
-  socket.on('createMessage', (newMessage) => {
-      console.log('new message: ',newMessage);
+  socket.on('createMessage', (message) => {
+      console.log('new message: ',message);
+      io.emit('newMessage', {
+        from: message.from,
+        text: message.text,
+        createdAt: new Date().getTime()
+      });
   });
 
   socket.on('disconnect', () => {
